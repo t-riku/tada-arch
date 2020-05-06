5dc05f0ebc3d9d13bed06229821255b7dc5692fe
多田陸 <tadariku@tadarikunoMacBook-Air.local>
2020-05-06 16:58:38 +0900

    news

--- src/News.tsx
+++ src/News.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+
 function News() {
     
         return(
             <div className='news'>
-                
+                <p>2020.4</p>
+                <a href="http://you-allabo.com/%e5%bb%ba%e7%af%89%e5%a3%ab%e3%82%92%e7%9b%ae%e6%8c%87%e3%81%99%e5%a5%b3%e5%ad%90%e5%ad%a6%e7%94%9f%e3%81%95%e3%82%93%e3%81%ae%e3%81%9f%e3%82%81%e3%81%ae%e3%82%aa%e3%83%b3%e3%83%a9%e3%82%a4%e3%83%b3/">建築士を目指す女子学生さんのためのオンライン質問会をおこないます（参加無料）</a>
+                <p>2020.1</p>
+                <a href= "https://peatix.com/group/7210577%E3%80%80">生き方見本市の詳細が決まりました。こちらから。</a>
+                <p>2020.1</p>
+                <a href= "https://events.asj-net.com/events/13389">謹賀新年。今年もよろしくお願いします。1月26日に長野県伊那文化会館で行われる渡辺篤史スペシャルトークショーに多田祐子が登壇いたします。お近くの方は是非お越しください。</a>
+                <p>2019.12</p>
+                <a href= "https://readyfor.jp/projects/ikikatatokai/announcements/120242">生き方見本市のクラウドファンディングが目標額達成！さらに学生支援のためのネクストゴールもクリアしました！生き方見本市は2月8日開催されますが、その他詳細が決まりましたらお知らせします。</a>
+                <p>2019.12</p>
+                <a href= "https://readyfor.jp/projects/ikikatatokai">生き方見本市TOKAIのイベント「千差万別の生き方に出会おう」に多田祐子が登壇いたします。クラウドファンディングは目標額ま でもう少しで達成です!興味のある方は是非ご参加下さい。</a>
+                <p>2019.10</p>
+                <a href= "http://aqua-marine.tea-nifty.com/arttada/2019/08/post-28cfc8.html">ライフスタイルに合わせた収納セミナーを行います。</a>
+                <p>2019.9</p>
+                <a href= "https://www.arttada.com/portfolio">作品に古民家リノベーション２件を含め、新たに合計６件の住宅をＵＰしました。</a>
+                <p>2019.8</p><p>私共は今後増え続ける、空き家問題に取り組んで参ります。利活用のご相談は問い合わせフォームまたはメールでどうぞ。</p>
+                <p>2019.6</p>
+                <a href= "https://youtu.be/W9-kZoBziw0">漆喰壁の汚れを高圧洗浄で綺麗に洗い流す、１２年点検補修工事＠鎌倉浄明寺の家</a>
+                <p>2019.5</p>
+                <a href= "https://youtu.be/-UPQRX_rDfE">満席御礼、つながるトークイベント、緊張をゆるりとほぐす芳しいコーヒー＠希望の家</a>
+                <p>2019.5</p>
+                <a href= "https://youtu.be/6Q3_9pZdfsM">祝上棟、高い技術力の女性クレーンオペ＠弥富の家</a>
+                <p>2019.5</p>
+                <a href= "https://youtu.be/_5yL8cHTofs">５月初め、今年も藤の花が見事に咲きました＠北鎌倉の家</a>
                 <div className='sns'>
-                    <p>aaa</p>
                 </div>
             </div>
         );
