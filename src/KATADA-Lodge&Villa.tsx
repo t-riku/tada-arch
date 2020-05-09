54d7f44f7340caad482d61ed1eb2470cd470e418
多田陸 <tadariku@tadarikunoMacBook-Air.local>
2020-05-09 15:35:56 +0900

    いろいろ

---
+++ src/KATADA-Lodge&Villa.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+function Lecture() {
+return(
+    <div>
+        <img src="https://www.arttada.com/application/files/7615/8800/5767/KATADA30.jpg"/>
+        <img src="https://www.arttada.com/application/files/9215/8800/5761/KATADA40.jpg"/>
+        <img src="https://www.arttada.com/application/files/5415/8800/5762/KATADA50.jpg"/>
+        <img src="https://www.arttada.com/application/files/2915/8800/5764/KATADA90.jpg"/>
+        <img src="https://www.arttada.com/application/files/8715/8800/5763/KATADA70.jpg"/>
+        <img src="https://www.arttada.com/application/files/9315/8800/5764/KATADA80.jpg"/>
+        <img src="https://www.arttada.com/application/files/3115/8800/5768/KATADA60.jpg"/>
+        <img src="https://www.arttada.com/application/files/9515/8800/5765/KATADA100.jpg"/>
+        <img src="https://www.arttada.com/application/files/3815/8800/5766/KATADA110.jpg"/>
+        <img src="https://www.arttada.com/application/files/8715/8800/5767/KATADA120.jpg"/>
+        <img src="https://www.arttada.com/application/files/8915/8800/5763/KATADA20.jpg"/>
+        <img src="https://www.arttada.com/application/files/1515/8800/5762/KATADA19.jpg"/>
+        <p>青い空が澄み渡る三重県津市の丘陵にある津カントリー倶楽部。その１８番ホール脇に建つKATADA Lodge&Villaは、まるでエメラルドグリーンの海に浮かぶ３槽の船のようだ。ツインルーム４室のロッジ棟、食事や団らんを楽しむラウンジ棟、１８番ホールの眺めを大スクリーンで眺めることのできるヴィラの３棟からなる。ゴルフ場のアンジュレーション（高低差）を活かし、各建物の間に出来る外部空間との心地よいリズム感をもたらしている。</p>
+        <p>外壁は神宮備林のある岐阜県加子母村産のヒノキを使用。建具はすべて木製建具、ライティングデスクや椅子、ベッド架台などのすべての家具は地元の家具職人の作品を採用した。また材料は木、紙、土、草などの自然素材を厳選。設計の段階から、現地まで足を運び、職人たちに会い、納得したものだけをチョイスしている。プラスチック製品、既製品を使用することを極力回避した。何よりも職人の手仕事、勘を大切にし、完成まで議論を重ね、その仕事ぶりを最大限に建築意匠に調和させている。</p>
+        <p>ピクチャーウィンドウとして様々なレヴェルで切り取られた美しい景色は、各室内に佇む人々の目を肥やし心を満たす。ヴィラは１８番ホールを目下とする場に配置。浴室、居間、寝室を大きく開放している。スラブを張り出し、浮遊感をもたせ、軒を深く出し、人の視線がしごく自然な形でグリーンをキャッチできるように計算している。</p>
+        <p>ラウンジは夕景を意識し、オレンジ色に染まった景色と馴染むよう、室内に使用する木の分量と配置を吟味した。まるで行灯の中に佇んでいるような感覚になる空間である。</p>
+        <p>ロッジは陶芸家/川喜田半泥子のアトリエのある長谷山と対峙しているのが最大の特徴だ。山の稜線と屋根勾配を合わせ、まるで自然と人工物が共鳴して息をしているかのような形態をとっている。建物中間に山に抜けるボイドを設け、単調になりがちな客室配置を避けながら緑と一体になる豊かな建築をうみだした。</p>
+    </div>
+ )
+}
---
+++ src/Works.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+function Lecture() {
+return(
+    <div>
+        <h1>宿泊施設</h1>
+        <a href="KATADA-Lodge&Villa.tsx"><img src="https://www.arttada.com/application/files/6215/8808/1469/1.jpg"/></a>
+        <h1>作品</h1>
+        <a href=""><img src="https://www.arttada.com/application/files/2615/6191/5525/sq.jpg"/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+        <a href=""><img src=""/></a>
+    </div>
+  )
+} 
