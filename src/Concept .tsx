fbb8de8965ac9a1e940bba831cfebfbd2cd37233
多田陸 <tadariku@tadarikunoMacBook-Air.local>
2020-05-10 00:36:50 +0900

    理念系

---
+++ src/Concept .tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+function Concept() {
+    return(
+        <div>
+            <h1>コンセプト</h1>
+            <img src="https://www.arttada.com/application/files/4914/7490/9959/835f1c84cb52af84c510b464b1e0ca43.jpg"/>
+            <p>“Simple&Comfortable”が私共の空間づくりに対する基本コンセプトです。</p>
+            <p>人間にとって空間とは、「美しさと快適さ」が基本だと考えます。</p>
+            <p>華美な美しさではなく洗練され、存在自体が美しいと言う意味です。</p>
+            <p>日本人が本来持ち合わせている感性をくすぐり、 心に響くようなデザインを心がけております。</p>
+            <p>快適さは、人間の五感に響くような空間づくり、「風、光、自然、香り、手触り」などをどのようにその空間に取り込むかを重視し計画を進めます</p>
+            <p>この2つがぴったりと重なり合った時、理想的な空間が生まれます。</p>
+            <p>光・緑・風を五感で感じること</p>
+            <p>人間として本当に心地よい空間・・・</p>
+            <p>あたりまえはあたりまえではない</p>
+            <p>じっくりと、ゆっくりと</p>
+            <p>時間と空間は変化しつづける</p>
+            <p>日本人が本来もつ感性</p>
+            <p>それはそれは豊かなもの</p>
+            <p>Wabi-Sabi</p>
+            <p>大切に残していきたい</p>
+            <p>"How marvelous that when I am in a room with another.</p>
+            <p>The mountains, trees, wind, and rain leave us for the mind,</p>
+            <p>and the room be come a world in itself."</p>
+            <p>「部屋の中に、他の人と一緒にいることは、何と素晴らしいことか、</p>
+            <p>その時、山も木も風も雨も心の中のものとなり、</p>
+            <p>部屋はそれ自体が一つの世界になる。」</p>
+            <p>イス・カーンル</p>
+        </div>
+    )
+}
